07e44cea5adf290d9f6347b02bb94d1ae178d1b2
Hyesu Hong <honghs95@gmail.com>
2021-08-03 01:43:00 +0900

    about class component, state, life cycle

--- src/App.js
+++ src/App.js
@@ -1,66 +1,60 @@
 import ProtTypes from 'prop-types';
+import React from 'react';
 
-function Fruits({ name, picture, rating }) {
-	return (
-		<div>
-			<h3>I Like {name}</h3>
-			<h5>{rating} / 5.0</h5>
-			<img src={picture} alt={name} />
-		</div>
-	);
-}
+// function component : 함수가 실행 -> 결과를 return -> 스크린에 디스플레이
+// function App() {
+// 	return (
+// 		<div className="App">
+// 			{fruitILike.map((item) => (
+// 				<Fruits
+// 					key={item.id}
+// 					name={item.name}
+// 					picture={item.image}
+// 					rating={item.rating}
+// 				/>
+// 			))}
+// 		</div>
+// 	);
+// }
 
-const fruitILike = [
-	{
-		id: 0,
-		name: 'Lemon',
-		image:
-			'https://images.unsplash.com/photo-1587496679742-bad502958fbf?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
-		rating: 4,
-	},
-	{
-		id: 1,
-		name: 'Peach',
-		image:
-			'https://images.unsplash.com/photo-1595124245030-41448b199d6d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVhY2h8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60',
-		rating: 5,
-	},
-	{
-		id: 2,
-		name: 'Orange',
-		image:
-			'https://images.unsplash.com/photo-1547514701-42782101795e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8b3JhbmdlJTIwZnJ1aXR8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60',
-		rating: 4,
-	},
-	{
-		id: 3,
-		name: 'Strawberry',
-		image:
-			'https://images.unsplash.com/photo-1589723933517-a08e544063cf?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8c3RyYXdiZXJyeXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60',
-		rating: 3.5,
-	},
-];
+// class component : react.component 에서 상속 -> 상속된 render method를 자동으로 실행 -> 결과를 return -> 스크린에 디스플레이
+class App extends React.Component {
+	state = {
+		count: 0,
+	};
 
-// to check props inside component
-Fruits.propTypes = {
-	name: ProtTypes.string.isRequired,
-	picture: ProtTypes.string.isRequired,
-	rating: ProtTypes.number,
-};
+	plus = () => {
+		// console.log('add');
+		this.setState((current) => ({ count: current.count + 1 }));
+	};
+	minus = () => {
+		// console.log('minus');
+		this.setState((current) => ({ count: current.count - 1 }));
+	};
 
-function App() {
-	return (
-		<div className="App">
-			{fruitILike.map((item) => (
-				<Fruits
-					key={item.id}
-					name={item.name}
-					picture={item.image}
-					rating={item.rating}
-				/>
-			))}
-		</div>
-	);
+	constructor(props) {
+		super(props);
+		console.log('before render');
+	}
+	componentDidMount() {
+		console.log('component rendered');
+	}
+	componentDidUpdate() {
+		console.log('I just updated');
+	}
+	componentWillUnmount() {
+		console.log('Good Bye');
+	}
+	render() {
+		console.log(`I'm rendering`);
+		return (
+			<div>
+				<h1>The number is {this.state.count}</h1>
+				<button onClick={this.plus}>Plus</button>
+				<button onClick={this.minus}>Minus</button>
+			</div>
+		);
+	}
 }
 
 export default App;
